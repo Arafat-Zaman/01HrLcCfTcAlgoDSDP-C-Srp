6108174d85d2baedfae4c4a0a85375ffc5e454a9
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2021-07-02 22:03:25 +0600

    DAY 11 _ 2D Array

---
+++ HackerRank/Day11_2DArrays.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Day11_2DArrays
+    {
+        static int sumHourglasses(int[][] arr, int i, int j)
+        {
+            return arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] +
+                arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int higherHourglasses = 0;
+            int currentCount = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    currentCount = sumHourglasses(arr, i, j);
+                    if (
+                        currentCount > higherHourglasses
+                        || (i == 0 && j == 0)
+                    )
+                        higherHourglasses = currentCount;
+                }
+            }
+
+            Console.WriteLine(higherHourglasses);
+        }
+    }
+}
